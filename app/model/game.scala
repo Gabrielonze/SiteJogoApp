8c97bb294e8c253556fbf9845c22412cfaab931c
Gabriel S. Alves <gabrielonze@hotmail.com>
2015-05-07 17:48:52 -0300

    Teste de funcionabilidade

--- app/model/game.scala
+++ app/model/game.scala
@@ -1,16 +1,22 @@
 import anorm._
+import play.api.db.DB
+import play.api.Play.current
+ 
 
-DB.withConnection { implicit c =>
-  val result: Boolean = SQL("Select 1").execute()    
-} 
 
 case class Game(id: Int, nome: String, descricao: String, nota: Float, genero: String)
-
+	
 object game{
 	
 	/*def findAll: List[Game]{
 
 	}*/
+
+
+
+	DB.withConnection { implicit c =>;
+		val result: Boolean = SQL("create table oi(id int)").execute()
+	}
 	
-	val result: Int = SQL("create table oi(id int)").execute()
+	
 }
